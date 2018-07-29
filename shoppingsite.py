5e159c6f799144fc5ddbe371377e40b4534a701e
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-29 13:01:13 -0700

    Added cookies!

--- shoppingsite.py
+++ shoppingsite.py
@@ -6,7 +6,7 @@ put melons in a shopping cart.
 Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 """
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, make_response, request
 import jinja2
 
 import melons
@@ -98,7 +98,11 @@ def add_to_cart(melon_id):
     # - flash a success message
     # - redirect the user to the cart page
 
-    return "Oops! This needs to be implemented!"
+    html = render_template("cart.html")
+    resp = make_response(html)
+    resp.set_cookie(melon_id)
+
+    return resp
 
 
 @app.route("/login", methods=["GET"])
